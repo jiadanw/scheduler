60484aaab7ee117f718a82fe503567db85f4a8a5
Jiadan <jiadanwang@gmail.com>
2019-08-15 23:07:11 +0000

    fixed bugs

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,24 +1,39 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, getAllByTestId, getByPlaceholderText, queryByText,getByAltText, getByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 import {waitForElement, fireEvent} from "@testing-library/react";
 
 afterEach(cleanup);
+describe("Application",()=> {
+
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText, debug } = render(<Application />);
+  const { getByText } = render(<Application />);
 
   return waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
-
-    debug();
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 });
 
+it("after booking, remaining spot is reduced by 1", async ()=> {
+  const {container} = render(<Application />);
+  await waitForElement(()=> {
+    return getByText(container, 'Archie Cohen')
+  })
+  const appointment = getAllByTestId(container, "appointment")[0];
+  fireEvent.click(getByAltText(appointment,"Add"));
+  fireEvent.change(getByPlaceholderText(appointment, /student name/i), 
+      {target:{value:"Lydia Miller-Jones"}
+    })
+  fireEvent.click(getByText(appointment, "Save"));
+  expect(getByAltText(appointment,"Processing")).toBeInTheDocument();
+  await waitForElement(()=> 
+  queryByText(appointment, "Lydia Miller-Hones"));
+  getAllByTestId(container, "Archie Cohen");
 
-
-
+  })
+})
